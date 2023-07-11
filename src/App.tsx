02974e45a433df721da059adba025c566f4f20c2
Meobot <nathanrhodes312@gmail.com>
2023-07-11 16:20:37 -0400

    types added

--- src/App.tsx
+++ src/App.tsx
@@ -6,27 +6,27 @@ import Planets from "./components/Planets";
 import Phenomena from "./components/Phenomena";
 
 function App() {
-  const [activeComponent, setActiveComponent] = useState("Home");
+	const [activeComponent, setActiveComponent] = useState<string>("Home");
 
-  const handleLinkClick = (component) => {
-    setActiveComponent(component);
-  };
+	const handleLinkClick = (component: string) => {
+		setActiveComponent(component);
+	};
 
-  let content;
-  if (activeComponent === "Home") {
-    content = <Home />;
-  } else if (activeComponent === "Planets") {
-    content = <Planets />;
-  } else if (activeComponent === "Phenomena") {
-    content = <Phenomena />;
-  }
+	let content;
+	if (activeComponent === "Home") {
+		content = <Home />;
+	} else if (activeComponent === "Planets") {
+		content = <Planets />;
+	} else if (activeComponent === "Phenomena") {
+		content = <Phenomena />;
+	}
 
-  return (
-    <div className="App">
-      <Navbar onLinkClick={handleLinkClick} />
-      {content}
-    </div>
-  );
+	return (
+		<div className="App">
+			<Navbar onLinkClick={handleLinkClick} />
+			{content}
+		</div>
+	);
 }
 
 export default App;
