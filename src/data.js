ffd6930ec5770e7f961538db4008ea4730a9fe34
Meobot <nathanrhodes312@gmail.com>
2023-07-12 08:37:44 -0400

    planet data started

---
+++ src/data.js
@@ -0,0 +1,11 @@
+const planetData = [
+	{
+		name: "Mercury",
+		description:
+			"Mercury is the smallest planet in the Solar System and the closest to the Sun. Its orbit around the Sun takes 87.97 Earth days, the shortest of all the planets in the Solar System. It is named after the Roman deity Mercury, the messenger of the gods.",
+		image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Mercury_in_color_-_Prockter07-edit1.jpg/220px-Mercury_in_color_-_Prockter07-edit1.jpg",
+		lengthOfDay: "175.9 Earth days",
+		averageTemperature: "167 °C",
+		numberOfMoons: "0",
+	},
+];
