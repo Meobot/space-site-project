b18c1685d66961cb265eb8c3cc087c5aec5423f9
Meobot <nathanrhodes312@gmail.com>
2023-07-12 08:50:15 -0400

    added planet data

--- src/data.js
+++ src/data.js
@@ -8,4 +8,76 @@ const planetData = [
 		averageTemperature: "167 °C",
 		numberOfMoons: "0",
 	},
+	{
+		name: "Venus",
+		description:
+			"Venus is the second planet from the Sun. It is named after the Roman goddess of love and beauty. As the brightest natural object in Earth's night sky after the Moon, Venus can cast shadows and can be, on rare occasion, visible to the naked eye in broad daylight.",
+		image: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/Venus-real_color.jpg/220px-Venus-real_color.jpg",
+		lengthOfDay: "116.75 Earth days",
+		averageTemperature: "464 °C",
+		numberOfMoons: "0",
+	},
+	{
+		name: "Earth",
+		description:
+			"Earth is the third planet from the Sun and the only astronomical object known to harbor life. About 29% of Earth's surface is land consisting of continents and islands. The remaining 71% is covered with water, mostly by oceans but also by lakes, rivers and other fresh water, which together constitute the hydrosphere.",
+		image: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/97/The_Earth_seen_from_Apollo_17.jpg/220px-The_Earth_seen_from_Apollo_17.jpg",
+		lengthOfDay: "24 hours",
+		averageTemperature: "15 °C",
+		numberOfMoons: "1",
+	},
+	{
+		name: "Mars",
+		description:
+			"Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System, being larger than only Mercury. In English, Mars carries the name of the Roman god of war and is often referred to as the 'Red Planet'.",
+		image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/OSIRIS_Mars_true_color.jpg/220px-OSIRIS_Mars_true_color.jpg",
+		lengthOfDay: "24.6 hours",
+		averageTemperature: "-65 °C",
+		numberOfMoons: "2",
+	},
+	{
+		name: "Jupiter",
+		description:
+			"Jupiter is the fifth planet from the Sun and the largest in the Solar System. It is a gas giant with a mass one-thousandth that of the Sun, but two-and-a-half times that of all the other planets in the Solar System combined.",
+		image: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2b/Jupiter_and_its_shrunken_Great_Red_Spot.jpg/220px-Jupiter_and_its_shrunken_Great_Red_Spot.jpg",
+		lengthOfDay: "9.9 hours",
+		averageTemperature: "-110 °C",
+		numberOfMoons: "79",
+	},
+	{
+		name: "Saturn",
+		description:
+			"Saturn is the sixth planet from the Sun and the second-largest in the Solar System, after Jupiter. It is a gas giant with an average radius of about nine times that of Earth. It only has one-eighth the average density of Earth.",
+		image: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Saturn_during_Equinox.jpg/220px-Saturn_during_Equinox.jpg",
+		lengthOfDay: "10.7 hours",
+		averageTemperature: "-140 °C",
+		numberOfMoons: "82",
+	},
+	{
+		name: "Uranus",
+		description:
+			"Uranus is the seventh planet from the Sun. Its name is a reference to the Greek god of the sky, Uranus according to Greek mythology, was the great-grandfather of Ares (Mars), grandfather of Zeus (Jupiter) and father of Cronus (Saturn).",
+		image: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/Uranus2.jpg/220px-Uranus2.jpg",
+		lengthOfDay: "17.2 hours",
+		averageTemperature: "-195 °C",
+		numberOfMoons: "27",
+	},
+	{
+		name: "Neptune",
+		description:
+			"Neptune is the eighth and farthest-known Solar planet from the Sun. In the Solar System, it is the fourth-largest planet by diameter, the third-most-massive planet, and the densest giant planet. It is 17 times the mass of Earth, slightly more massive than its near-twin Uranus.",
+		image: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Neptune_Full.jpg/220px-Neptune_Full.jpg",
+		lengthOfDay: "16.1 hours",
+		averageTemperature: "-200 °C",
+		numberOfMoons: "14",
+	},
+	{
+		name: "Pluto",
+		description:
+			"Pluto is an icy dwarf planet in the Kuiper belt, a ring of bodies beyond the orbit of Neptune. It was the first Kuiper belt object to be discovered and is the largest known plutoid. Pluto was discovered by Clyde Tombaugh in 1930 as the ninth planet from the Sun.",
+		image: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Pluto_in_True_Color_-_High-Res.jpg/220px-Pluto_in_True_Color_-_High-Res.jpg",
+		lengthOfDay: "153.3 hours",
+		averageTemperature: "-225 °C",
+		numberOfMoons: "5",
+	},
 ];
